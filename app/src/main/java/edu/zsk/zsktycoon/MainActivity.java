e49e44fbbde5db89c0a52e8d8037ab536243f275
Stanisław <stachupiwko@gmail.com>
2026-04-27 20:49:28 +0200

    Added string and style files and made progress bar a moving train/tram

--- app/src/main/java/edu/zsk/zsktycoon/MainActivity.java
+++ app/src/main/java/edu/zsk/zsktycoon/MainActivity.java
@@ -3,15 +3,22 @@ package edu.zsk.zsktycoon;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ClipDrawable tramClip;
+    private ClipDrawable train1Clip;
+    private ClipDrawable train2Clip;
+    private ClipDrawable train3Clip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,76 +26,91 @@ public class MainActivity extends AppCompatActivity {
 
         GameView gm = GameView.getInstance();
 
-        TextView avenidaText  = findViewById(R.id.avenida_text);
-        TextView schoolText   = findViewById(R.id.school_text);
-        TextView teacherText  = findViewById(R.id.teacher_text);
+        TextView avenidaText = findViewById(R.id.avenida_text);
+        TextView schoolText = findViewById(R.id.school_text);
+        TextView teacherText = findViewById(R.id.teacher_text);
 
-        ProgressBar tramProgress   = findViewById(R.id.tram_progress);
-        TextView    tramStatus     = findViewById(R.id.tram_status);
-        ImageView   tramIcon       = findViewById(R.id.tram_progress_icon);
 
-        ProgressBar trainProgress1 = findViewById(R.id.train_progress);
-        TextView    trainStatus1   = findViewById(R.id.train_status);
-        ImageView   train1Icon     = findViewById(R.id.train1_progress_icon);
+        ImageView tramImage = findViewById(R.id.tram_image);
+        ImageView train1Image = findViewById(R.id.train1_image);
+        ImageView train2Image = findViewById(R.id.train2_image);
+        ImageView train3Image = findViewById(R.id.train3_image);
 
-        ProgressBar trainProgress2 = findViewById(R.id.train_progress2);
-        TextView    trainStatus2   = findViewById(R.id.train_status2);
+        TextView tramStatus = findViewById(R.id.tram_status);
+        TextView trainStatus1 = findViewById(R.id.train_status1);
+        TextView trainStatus2 = findViewById(R.id.train_status2);
+        TextView trainStatus3 = findViewById(R.id.train_status3);
 
-        ProgressBar trainProgress3 = findViewById(R.id.train_progress3);
-        TextView    trainStatus3   = findViewById(R.id.train_status3);
+        LinearLayout train2Card = findViewById(R.id.train2_card);
+        LinearLayout train3Card = findViewById(R.id.train3_card);
 
-        LinearLayout train2Card    = findViewById(R.id.train2_card);
-        LinearLayout train3Card    = findViewById(R.id.train3_card);
-
-        Button newTrainBtn     = findViewById(R.id.new_train_btn);
-        Button tramUpgradeBtn  = findViewById(R.id.tram_upgrade_btn);
+        Button newTrainBtn = findViewById(R.id.new_train_btn);
+        Button tramUpgradeBtn = findViewById(R.id.tram_upgrade_btn);
         Button train1UpgradeBtn = findViewById(R.id.train1_upgrade_btn);
         Button train2UpgradeBtn = findViewById(R.id.train2_upgrade_btn);
         Button train3UpgradeBtn = findViewById(R.id.train3_upgrade_btn);
 
-        // --- Dane ---
-        gm.avenidaStudents.observe(this, val -> avenidaText.setText(String.valueOf(val)));
-        gm.schoolStudents .observe(this, val -> schoolText .setText(String.valueOf(val)));
-        gm.teachers       .observe(this, val -> teacherText.setText("Nauczyciele: " + val));
 
-        // --- Tramwaj ---
+        tramClip = (ClipDrawable) tramImage.getDrawable();
+        train1Clip = (ClipDrawable) train1Image.getDrawable();
+        train2Clip = (ClipDrawable) train2Image.getDrawable();
+        train3Clip = (ClipDrawable) train3Image.getDrawable();
+
+
+        gm.avenidaStudents.observe(this, val -> avenidaText.setText(String.valueOf(val)));
+        gm.schoolStudents.observe(this, val -> schoolText.setText(String.valueOf(val)));
+        gm.teachers.observe(this, val -> teacherText.setText("Nauczyciele: " + val));
+
+
         gm.tramProgress.observe(this, val -> {
-            tramProgress.setProgress(val);
-            moveIconWithProgress(tramProgress, tramIcon, val);
+            if (tramClip != null) {
+                int level = val * 100;  // 0-100 -> 0-10000
+                tramClip.setLevel(level);
+            }
         });
         gm.tramStatus.observe(this, tramStatus::setText);
 
-        // --- Pociąg 1 ---
+
         gm.train1Progress.observe(this, val -> {
-            trainProgress1.setProgress(val);
-            moveIconWithProgress(trainProgress1, train1Icon, val);
+            if (train1Clip != null) {
+                train1Clip.setLevel(val * 100);
+            }
         });
         gm.train1Status.observe(this, trainStatus1::setText);
 
-        // --- Pociąg 2 ---
-        gm.train2Progress.observe(this, trainProgress2::setProgress);
-        gm.train2Status  .observe(this, trainStatus2::setText);
 
-        // --- Pociąg 3 ---
-        gm.train3Progress.observe(this, trainProgress3::setProgress);
-        gm.train3Status  .observe(this, trainStatus3::setText);
+        gm.train2Progress.observe(this, val -> {
+            if (train2Clip != null) {
+                train2Clip.setLevel(val * 100);
+            }
+        });
+        gm.train2Status.observe(this, trainStatus2::setText);
 
-        // Widoczność kart
+
+        gm.train3Progress.observe(this, val -> {
+            if (train3Clip != null) {
+                train3Clip.setLevel(val * 100);
+            }
+        });
+        gm.train3Status.observe(this, trainStatus3::setText);
+
+
         gm.train2Owned.observe(this, owned ->
                 train2Card.setVisibility(owned ? View.VISIBLE : View.GONE));
         gm.train3Owned.observe(this, owned ->
                 train3Card.setVisibility(owned ? View.VISIBLE : View.GONE));
 
-        // --- Kupno nowej linii ---
-        gm.nextTrainCost  .observe(this, cost ->
-                newTrainBtn.setText("🚆 Kup nową linię (" + cost + " uczniów)"));
+
+        gm.nextTrainCost.observe(this, cost ->
+                newTrainBtn.setText("Kup nową linię (" + cost + " uczniów)"));
         gm.ownedTrainsCount.observe(this, cnt -> {
             newTrainBtn.setEnabled(cnt < 3);
-            if (cnt >= 3) newTrainBtn.setText("Wszystkie linie kupione ✓");
+            if (cnt >= 3) newTrainBtn.setText("Wszystkie linie kupione");
         });
         newTrainBtn.setOnClickListener(v -> gm.buyNewTrain());
 
-        tramUpgradeBtn .setOnClickListener(v -> startActivity(new Intent(this, TramUpgradeActivity.class)));
+        tramUpgradeBtn.setOnClickListener(v ->
+                startActivity(new Intent(this, TramUpgradeActivity.class)));
 
         train1UpgradeBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, TrainUpgradeActivity.class);
@@ -107,49 +129,5 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("nbr", 3);
             startActivity(intent);
         });
-        newTrainBtn.setOnClickListener(v -> gm.buyNewTrain());
-
-        tramUpgradeBtn.setOnClickListener(v ->
-                startActivity(new Intent(this, TramUpgradeActivity.class)));
-        trainUpgradeBtn.setOnClickListener(v ->
-                startActivity(new Intent(this, TrainUpgradeActivity.class)));
-    }
-
-    /**
-     * Przesuwa ikonę (tramwaj/pociąg) wzdłuż paska postępu.
-     *
-     * Jak dodać własny obrazek:
-     * 1. Skopiuj plik tram.png / train.png do res/drawable/
-     * 2. W activity_main.xml zmień android:src na @drawable/tram lub @drawable/train
-     *
-     * @param bar      ProgressBar po którym jeździ ikona
-     * @param icon     ImageView z ikonką pojazdu
-     * @param progress wartość 0-100
-     */
-    private void moveIconWithProgress(ProgressBar bar, ImageView icon, int progress) {
-        if (icon == null) return;
-
-        // Czekaj aż widok będzie zmierzony
-        if (bar.getWidth() == 0) {
-            bar.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                @Override
-                public void onGlobalLayout() {
-                    bar.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                    applyIconPosition(bar, icon, progress);
-                }
-            });
-        } else {
-            applyIconPosition(bar, icon, progress);
-        }
-    }
-
-    private void applyIconPosition(ProgressBar bar, ImageView icon, int progress) {
-        float ratio    = progress / 100f;
-        float barWidth = bar.getWidth();
-        float iconW    = icon.getWidth() > 0 ? icon.getWidth() : 56f;
-
-        // Pozycja X ikony: od lewego brzegu paska do miejsca odpowiadającego progress
-        float x = bar.getX() + ratio * (barWidth - iconW);
-        icon.setX(Math.max(bar.getX(), x));
     }
 }
